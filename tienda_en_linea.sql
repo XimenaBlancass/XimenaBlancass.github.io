f2b396d01b073934e81e6baca61f46a24a58a037
Ximena Blancas <124410723+XimenaBlancass@users.noreply.github.com>
2023-03-24 19:50:58 -0600

    Practica 14/ 24.03.23

---
+++ tienda_en_linea.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-03-2023 a las 02:50:01
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tienda en linea`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Maquillaje', 'Compra productos de maquillaje. Eleva tu rutina de belleza con maquillaje y cosméticos de las mejore'),
+(2, 'Ropa', 'Explora en línea nuestra variedad de prendas recién llegadas. Siempre encontrarás las últimas tenden'),
+(3, 'Tenis', 'Encuentra la mejor selección de tenis en nuestra tienda en línea. Luce tu estilo urbano, ¡tenemos el'),
+(4, 'Computadoras', 'Encuentra productos de computación y las mejores computadoras a precios increíbles: accesorios, note'),
+(5, 'Celulares', 'Encontrá lo que buscás en Celulares y Telefonía. Todo lo que necesitas lo conseguís en un solo lugar'),
+(6, 'Maquillaje', 'Compra productos de maquillaje. Eleva tu rutina de belleza con maquillaje y cosméticos de las mejore'),
+(7, 'Ropa', 'Explora en línea nuestra variedad de prendas recién llegadas. Siempre encontrarás las últimas tenden'),
+(8, 'Tenis', 'Encuentra la mejor selección de tenis en nuestra tienda en línea. Luce tu estilo urbano, ¡tenemos el'),
+(9, 'Computadoras', 'Encuentra productos de computación y las mejores computadoras a precios increíbles: accesorios, note'),
+(10, 'Celulares', 'Encontrá lo que buscás en Celulares y Telefonía. Todo lo que necesitas lo conseguís en un solo lugar');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `nombre` int(150) NOT NULL,
+  `precio` float NOT NULL,
+  `categoria_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tienda en linea`
+--
+
+CREATE TABLE `tienda en linea` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria_id` (`categoria_id`);
+
+--
+-- Indices de la tabla `tienda en linea`
+--
+ALTER TABLE `tienda en linea`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tienda en linea`
+--
+ALTER TABLE `tienda en linea`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
